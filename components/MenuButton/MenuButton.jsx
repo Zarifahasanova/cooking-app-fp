587194744ba2c3ae808c52249ba6ba0a2a830007
javidahmadov <cavidahmadov111@gmail.com>
2020-07-02 17:00:25 +0400

    back button, menu button added

--- components/MenuButton/MenuButton.jsx
+++ components/MenuButton/MenuButton.jsx
@@ -20,28 +20,7 @@ export default class MenuButton extends React.Component {
   }
 }
 
-// export  const styles = StyleSheet.create({
-//     btnClickContain: {
-//       flexDirection: 'row',
-//       padding: 5,
-//       marginTop: 5,
-//       marginBottom: 5
-//     },
-//     btnContainer: {
-//       flex: 1,
-//       flexDirection: 'row',
-//       alignItems: 'flex-start'
-//     },
-//     btnIcon: {
-//       height: 25,
-//       width: 25
-//     },
-//     btnText: {
-//       fontSize: 16,
-//       marginLeft: 10,
-//       marginTop: 2
-//     }
-//   });
+
   
 MenuButton.propTypes = {
   onPress: PropTypes.func,
